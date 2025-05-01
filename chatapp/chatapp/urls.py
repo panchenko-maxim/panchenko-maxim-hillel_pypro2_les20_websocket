99f632debc8a69b7b4085b3d0ddb95a252829582
root221 <panchenko.maxim87@gmail.com>
2025-05-01 16:04:45 +0300

    lesson 23 - end

--- chatapp/chatapp/urls.py
+++ chatapp/chatapp/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+from chatapp.chatapp.settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +28,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('login/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
